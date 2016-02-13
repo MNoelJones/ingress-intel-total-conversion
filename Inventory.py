dca4890783ef6b2744fb81ee2e74bb6b02e751dc
MNoelJones <mnjones+git@gmail.com>
2016-02-13 22:29:09 +0000

    Inventory.py

---
+++ Inventory.py
@@ -0,0 +1,371 @@
+import re
+
+
+class Item(object):
+    """ A virtual class container for all Inventory item types """
+    def __init__(self):
+        """ level: all items have a level... don't they? """
+        self._level = None
+        self._shortcode = None
+
+    @property
+    def level(self):
+        """ Item Level """
+        return self._level
+
+    @level.setter
+    def level(self, lvl):
+        self._level = lvl
+
+    def itemcount(self):
+        return 1
+
+    def invcount(self):
+        return 1
+
+
+class Rarity(object):
+    """
+        Some items have a Rarity value.
+        This is a virtual class for all Rarity levels
+    """
+    def __init__(self):
+        self._shortcode = None
+
+
+class Common(Rarity):
+    """ Very Rare level rarity """
+    def __init__(self):
+        super(Common, self).__init__()
+        self._shortcode = "VR"
+
+
+class Rare(Rarity):
+    """ Rare level rarity """
+    def __init__(self):
+        super(Rare, self).__init__()
+        self._shortcode = "VR"
+
+
+class VeryRare(Rarity):
+    """ Very Rare level rarity """
+    def __init__(self):
+        super(VeryRare, self).__init__()
+        self._shortcode = "VR"
+
+
+class ItemWithRarity(Item):
+    def __init__(self):
+        super(ItemWithRarity, self).__init__()
+        self._rarity = None
+
+    @property
+    def rarity(self):
+        return self._rarity
+
+    @rarity.setter
+    def rarity(self, rarity):
+        self._rarity = rarity
+
+
+class Key(Item):
+    """ """
+    def __init__(self):
+        super(Key, self).__init__()
+
+
+class Media(Item):
+    """ """
+    def __init__(self):
+        super(Media, self).__init__()
+
+
+class Powerup(Item):
+    """ """
+    def __init__(self):
+        super(Powerup, self).__init__()
+
+
+class PowerCube(ItemWithRarity):
+    """ """
+    def __init__(self):
+        super(PowerCube, self).__init__()
+
+
+class Resonator(ItemWithRarity):
+    """ """
+    def __init__(self):
+        super(Resonator, self).__init__()
+
+
+class Container(ItemWithRarity):
+    def __init__(self, id=None):
+        super(Container, self).__init__()
+        self._id = id or None
+        self._contents = []
+        # Array of valid items for this container
+        self._restricted_items = []
+
+    @property
+    def contents(self):
+        return self._contents
+
+    @contents.setter
+    def contents(self, val):
+        self._contents = val
+
+    def itemcount(self):
+        return len(self.contents) + 1
+
+    def invcount(self):
+        return self.itemcount()
+
+    def add(self, item):
+        assert(any([isinstance(item, x) for x in self._restricted_items]))
+        self.contents.append(item)
+
+
+class MUFG(Container):
+    """ """
+    def __init__(self):
+        super(Capsule, self).__init__()
+        self._restricted_items = [
+            Resonator,
+            Key,
+            Media,
+            PowerCube,
+            Mod,
+            Weapon,
+        ]
+
+
+class Capsule(Container):
+    """ """
+    def __init__(self):
+        super(Capsule, self).__init__()
+        self._restricted_items = [
+            Resonator,
+            Key,
+            Media,
+            PowerCube,
+            Mod,
+            Weapon,
+        ]
+
+
+class KeyCapsule(Container):
+    def __init__(self):
+        super(KeyCapsule, self).__init__()
+        self._restricted_items = [
+            Key,
+        ]
+
+    def invcount(self):
+        return 1
+
+
+class Weapon(ItemWithRarity):
+    """ """
+    def __init__(self):
+        super(Weapon, self).__init__()
+
+
+class Burster(Weapon):
+    """ """
+    def __init__(self):
+        super(Burster, self).__init__()
+
+
+class Ultrastrike(Weapon):
+    """ """
+    def __init__(self):
+        super(Ultrastrike, self).__init__()
+
+
+class Virus(Weapon):
+    """ """
+    def __init__(self):
+        super(Virus, self).__init__()
+
+
+class Ada(Virus):
+    """ """
+    def __init__(self):
+        super(Ada, self).__init__()
+
+
+class Jarvis(Virus):
+    """ """
+    def __init__(self):
+        super(Jarvis, self).__init__()
+
+
+class Mod(ItemWithRarity):
+    """ """
+    def __init__(self):
+        super(Mod, self).__init__()
+
+
+class MultiHack(Mod):
+    """ """
+    def __init__(self):
+        super(MultiHack, self).__init__()
+
+
+class ForceAmp(Mod):
+    """ """
+    def __init__(self):
+        super(ForceAmp, self).__init__()
+
+
+class Turret(Mod):
+    """ """
+    def __init__(self):
+        super(Turret, self).__init__()
+
+
+class Shield(Mod):
+    """ """
+    def __init__(self):
+        super(Shield, self).__init__()
+
+
+class AxaShield(Shield):
+    def __init__(self):
+        super(AxaShield, self).__init__()
+        self.rarity = VeryRare()
+
+
+class LinkAmp(Mod):
+    """ """
+    def __init__(self):
+        super(LinkAmp, self).__init__()
+
+
+class SoftBankUltraLink(LinkAmp):
+    def __init__(self):
+        super(SoftBankUltraLink, self).__init__()
+        self.rarity = VeryRare()
+
+
+class HeatSink(Mod):
+    """ """
+    def __init__(self):
+        super(HeatSink, self).__init__()
+
+
+class Inventory(object):
+    def __init__(self):
+        self._inventory = []
+        self._shortcodes = {
+            'X': Burster,
+            'P': PowerCube,
+            'MH': MultiHack,
+            'HS': HeatSink,
+            'S': Shield,
+            'FA': ForceAmp,
+            'LA': LinkAmp,
+            'T': Turret,
+            'R': Resonator,
+            'US': Ultrastrike
+        }
+        self._raritycodes = {
+            'C': Common,
+            'R': Rare,
+            'VR': VeryRare
+        }
+
+    @property
+    def inventory(self):
+        return self._inventory
+
+    @property
+    def resonators(self):
+        return [x for x in self._inventory if isinstance(x, Resonator)]
+
+    @property
+    def weapons(self):
+        return [x for x in self._inventory if isinstance(x, Weapon)]
+
+    @property
+    def bursters(self):
+        return [x for x in self.weapons if isinstance(x, Burster)]
+
+    @property
+    def mods(self):
+        return [x for x in self._inventory if isinstance(x, Mod)]
+
+    @property
+    def powercubes(self):
+        return [x for x in self._inventory if isinstance(x, PowerCube)]
+
+    def itemcount(self):
+        return reduce(lambda a, b: a+b.itemcount(), self.inventory, 0)
+
+    def invcount(self):
+        return reduce(lambda a, b: a+b.invcount(), self.inventory, 0)
+
+    def add(self, item):
+        self.inventory.append(item)
+
+    def delete(self, item):
+        try:
+            for elem in self.inventory:
+                try:
+                    # identify item by guid and remove it.
+                    if elem.guid == item.guid:
+                        self.inventory.remove(elem)
+                        break
+                except AttributeError:
+                    if (item.__class__ == elem.__class__ and
+                       item.level == elem.level and
+                       item.rarity == elem.rarity):
+                        self.inventory.remove(elem)
+                        break
+            else:
+                raise RuntimeError(
+                    "Could not find item with guid {} in inventory".format(
+                        item.guid
+                    )
+                )
+        except ValueError:
+            pass
+
+    def apply_transaction(self, transaction):
+        tx_re = re.compile(
+            (
+             r'(?P<crdr>CR|DR)\s+(?P<target>\w+)\s+'
+             r'(?P<transaction>(?:\d+\s+(?:{})\d\s*)+)'
+            ).format('|'.join(x for x in self._shortcodes))
+        )
+        operations = {
+            "CR": "add",
+            "DR": "delete"
+        }
+        for match in tx_re.finditer(transaction):
+            target = self
+            if match.group("target") != "INV":
+                target = match.group("target")
+            self._apply_individual_transaction(
+                operations[match.group("crdr")],
+                target,
+                match.group("transaction")
+            )
+
+    def _apply_individual_transaction(self, operation, target, transaction):
+        tx_re = re.compile(
+            r'(?P<amount>\d+)\s+(?P<shortcode>(?:{}))(?P<level>\d)\s*'.format(
+                '|'.join(x for x in self._shortcodes)
+            )
+        )
+
+        for match in tx_re.finditer(transaction):
+            for _ in range(0, int(match.group("amount"))):
+                klass = self._shortcodes[match.group("shortcode")]
+                item = klass()
+                item.level = int(match.group("level"))
+                try:
+                    item.rarity = self._raritycodes[match.group("rarity")]
+                except IndexError:
+                    pass
+                getattr(target, operation)(item)
---
+++ Inventory_test.py
@@ -0,0 +1,107 @@
+# Inventory BDD Tests
+from pyspecs import given, when, the, then, and_, however
+import Inventory
+
+# It should allow you to add any Item to it
+with given.an_empty_inventory:
+    inventory = Inventory.Inventory()
+    with then.the_inventory_size_should_be_zero:
+        the(inventory.invcount()).should.equal(0)
+
+    with when.adding_a_mod:
+        item = Inventory.Mod()
+        inventory.add(item)
+
+        with then.the_size_should_be_one:
+            the(inventory.invcount()).should.equal(1)
+
+if False:
+    with given.an_item:
+        with then.insantiating_the_Item_raises_an_error:
+            the(Inventory.Item).should.raise_a(TypeError)
+
+with given.a_populated_inventory:
+    inventory = Inventory.Inventory()
+    counts = {
+        "Resonator": 10,
+        "Mod": 5
+    }
+    for item, num in counts.iteritems():
+        for _ in range(0, num):
+            inventory.add(getattr(Inventory, item)())
+
+    with then.inventory_count_is_15:
+        the(inventory.invcount()).should.equal(15)
+
+    with then.inventory_count_of_resonators_is_10:
+        the(
+            reduce(lambda a, b: a+b.invcount(), inventory.resonators, 0)
+        ).should.equal(10)
+    with then.inventory_count_of_mods_is_5:
+        the(
+            reduce(lambda a, b: a+b.invcount(), inventory.mods, 0)
+        ).should.equal(5)
+
+    with then.immediate_inventory_count_is_15:
+        the(inventory.invcount()).should.equal(15)
+
+    with when.adding_a_capsule_with_5_items:
+        capsule = Inventory.Capsule()
+        for _ in range(0, 5):
+            capsule.add(Inventory.Resonator())
+        inventory.add(capsule)
+
+        with and_.both_counts_are_21:
+            the(inventory.itemcount()).should.equal(21)
+            the(inventory.invcount()).should.equal(21)
+
+    with when.adding_a_keylocker_with_5_keys:
+        locker = Inventory.KeyCapsule()
+        for _ in range(0, 5):
+            locker.add(Inventory.Key())
+        inventory.add(locker)
+
+        with then.itemcount_is_27:
+            the(inventory.itemcount()).should.equal(27)
+        with however.inventory_count_should_equal_22:
+            the(inventory.invcount()).should.equal(22)
+
+with given.a_L8_burster:
+    burster = Inventory.Burster()
+    burster.level = 8
+    with then.the_level_should_report_8:
+        the(burster.level).should.equal(8)
+
+with given.adding_inventory_from_a_transaction_specification_for_5_L8_Bursters:
+    inventory = Inventory.Inventory()
+    transaction = "CR INV 5 X8"
+    inventory.apply_transaction(transaction)
+    with then.the_inventory_should_have_five_Bursters:
+        the(len(inventory.bursters)).should.equal(5)
+    with then.the_bursters_should_all_be_level_eight:
+        the(all(x.level == 8 for x in inventory.bursters)).should.be(True)
+
+with given.adding_from_a_transaction_specification_for_multiple_items:
+    inventory = Inventory.Inventory()
+    transaction = "CR INV 5 X8 3 R6"
+    inventory.apply_transaction(transaction)
+    with and_.the_inventory_should_have_the_correct_number_of_each_item:
+        the(len(inventory.bursters)).should.equal(5)
+        the(len(inventory.resonators)).should.equal(3)
+    with and_.the_items_should_be_the_correct_level:
+        the(all(x.level == 8 for x in inventory.bursters)).should.be(True)
+        the(all(x.level == 6 for x in inventory.resonators)).should.be(True)
+
+with given.using_a_debit_transation:
+    # Add 5 X8s so we can remove some of them
+    inventory = Inventory.Inventory()
+    transaction = "CR INV 5 X8"
+    inventory.apply_transaction(transaction)
+    # Remove 3 of the added bursters
+    transaction = "DR INV 3 X8"
+    inventory.apply_transaction(transaction)
+
+    with then.the_inventory_should_have_two_Bursters:
+        the(len(inventory.bursters)).should.equal(2)
+    with then.the_bursters_should_all_be_level_eight:
+        the(all(x.level == 8 for x in inventory.bursters)).should.be(True)
