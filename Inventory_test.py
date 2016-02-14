ec1f3ef84628906709d5dfe23091cc3a7f8686fd
MNoelJones <mnjones+git@gmail.com>
2016-02-14 16:44:09 +0000

    Add failing test for staged transactions

--- Inventory_test.py
+++ Inventory_test.py
@@ -128,3 +128,24 @@ with given.a_debit_transation_for_a_guid_item:
                 for x in inventory.capsules["9FD860A1"]
             )
         ).should.be(True)
+
+with given.a_stored_transaction:
+    inventory = Inventory.Inventory()
+    capsule = Inventory.Capsule()
+    capsule.guid = "9FD860A1"
+    inventory.add(capsule)
+    inventory.apply_transaction("CR INV 5 X8 10 R1")
+    inventory.apply_transaction("CR 9FD860A1 10 X8")
+    inventory.stage_transaction("DR INV 5 X8")
+    inventory.stage_transaction("CR 9FD860A1 5 X8")
+    with and_.the_inventory_contents_should_be_correct:
+        the(len(inventory.resonators())).should.equal(10)
+        the(len(inventory.bursters())).should.equal(5)
+        the(len(inventory.capsules["9FD860A1"].bursters)).should.equal(10)
+
+    with then.applying_staged_transactions:
+        inventory.apply_staged_transactions()
+        with and_.the_inventory_contents_should_be_correct:
+            the(len(inventory.resonators())).should.equal(10)
+            the(len(inventory.bursters())).should.equal(0)
+            the(len(inventory.capsules["9FD860A1"].bursters)).should.equal(15)
